05c6fb60b476a57760244e8fce2cc398b0ac5881
cpesin <christophe.pesin@gmail.com>
2023-03-13 16:26:31 +0100

    Fix phpcs error

--- src/Form/Admin/AuthorType.php
+++ src/Form/Admin/AuthorType.php
@@ -6,12 +6,12 @@ namespace App\Form\Admin;
 
 use App\Entity\Author;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 /**
  * [Description AuthorType].
