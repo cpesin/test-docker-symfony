277456b183450db2ca1f222de7456beb0a99ee7d
cpesin <christophe.pesin@gmail.com>
2023-03-14 14:08:01 +0100

    Add webpack encore

---
+++ assets/app.js
@@ -0,0 +1,23 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/bootstrap.min.scss';
+import './styles/bootstrap-reboot.min.scss';
+import './styles/bootstrap-grid.min.scss';
+import './styles/bootstrap-utilities.min.scss';
+import './styles/app.scss';
+
+// start the Stimulus application
+import './bootstrap';
+
+// Bootstrap
+import './bootstrap.min';
+import './bootstrap.bundle.min';
+
+// Other scripts
+import './scripts';
